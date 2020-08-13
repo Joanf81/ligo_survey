eda74541240d5d7d3cf6020dee9e8fe87c4634db
Joan Fernández Bornay <joanf81@gmail.com>
2020-08-13 15:16:02 +0200

    [#2] Added view to answer the questions

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -28,7 +28,7 @@ class QuizzesController < ApplicationController
 
     respond_to do |format|
       if @quiz.save
-        format.html { redirect_to edit_quiz_path(@quiz), notice: 'Quiz was successfully created.' }
+        format.html { redirect_to quiz_questions_path(@quiz), notice: 'Quiz was successfully created.' }
         # format.json { render :show, status: :created, location: @quiz }
       else
         format.html { render :new }
