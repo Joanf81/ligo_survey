b7dfa5c369aec03685d3a5cd563970b7312c90ec
Joan Fernández Bornay <joanf81@gmail.com>
2020-08-13 13:08:54 +0200

    [#2] Set DB relation: Question have many answers

--- db/migrate/20200813110110_create_answers.rb
+++ db/migrate/20200813110110_create_answers.rb
@@ -2,6 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.string :answer
+      t.belongs_to :question
 
       t.timestamps
     end
