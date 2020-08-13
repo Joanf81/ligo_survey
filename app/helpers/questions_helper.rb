6610e087f893f05c711454b496c8a31844b1eb1d
Joan Fernández Bornay <joanf81@gmail.com>
2020-08-13 19:49:29 +0200

    [#3] Added links for previous and next question

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,5 +1,21 @@
 module QuestionsHelper
 	def get_selected_answer
-    @quiz.get_selected_answer_for_question(@question)
-  end
+	    @quiz.get_selected_answer_for_question(@question)
+	end
+
+	def next_question_path
+		quiz_question_path(@quiz, @question.next_question)
+	end
+
+	def previous_question_path
+		quiz_question_path(@quiz, @question.prev_question)
+	end
+
+	def next?
+		@question.next_question?
+	end
+
+	def prev?
+		@question.prev_question?
+	end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,19 @@
 class Question < ApplicationRecord
 	has_many :answers
+
+	def next_question
+	    @next_question ||= Question.where("id > ?", id).try(:first)
+	end
+
+	def prev_question
+	    @prev_question ||= Question.where("id < ?", id).try(:last)
+	end
+
+	def next_question?
+		!next_question.nil?
+	end
+
+	def prev_question?
+		!prev_question.nil?
+	end
 end
